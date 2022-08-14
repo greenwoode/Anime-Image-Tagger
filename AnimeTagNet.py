b0067af604609341502fb009faf6c87fc69fdda2
Ethan Greenwood <Ethan.a.Greenwood@gmail.com>
2022-08-14 13:25:11 -0400

    added CNN
    current error, believed to be due to some images importing as grayscale

--- AnimeTagNet.py
+++ AnimeTagNet.py
@@ -1,10 +1,7 @@
 import numpy as np
-import pandas as pd
-#import tensorflow as tf
 import os
 import json
-#import sympy
-#import psutil
+import psutil
 
 metaFiles = []
 metaDir = ".\\metadata"
@@ -12,159 +9,357 @@ newMetaDir = ".\\validMeta"
 imagesDir = ".\\images"
 n = 337038
 
-_PROCESSED_JSONS_ = True
+_PREPROCESS_ = False
 
-if not _PROCESSED_JSONS_:
-    for filePath in os.listdir(metaDir):
-        metaFiles.append(os.path.join(metaDir, filePath))
+if _PREPROCESS_:
 
-    print("found ", len(metaFiles), " metadata files")
+    _PROCESS_JSONS_ =  False
 
-    foundTotal = 0
-    missedTotal = 0
-    for JSON in metaFiles:
+    if _PROCESS_JSONS_:
+        for filePath in os.listdir(metaDir):
+            metaFiles.append(os.path.join(metaDir, filePath))
 
-        metaFile = open(JSON, 'r', encoding="utf-8")
-        newMetaFile = open(JSON.replace(metaDir, newMetaDir), 'w+', encoding="utf-8")
+        print("found ", len(metaFiles), " metadata files")
+
+        foundTotal = 0
+        missedTotal = 0
+        for JSON in metaFiles:
+
+            metaFile = open(JSON, 'r', encoding="utf-8")
+            newMetaFile = open(JSON.replace(metaDir, newMetaDir), 'w+', encoding="utf-8")
 
 
-        found = 0
-        missed = 0
-        Lines = metaFile.readlines()
-        total = len(Lines)
-        for line in Lines:
+            found = 0
+            missed = 0
+            Lines = metaFile.readlines()
+            total = len(Lines)
+            for line in Lines:
 
-            metadata = json.loads(line)
-            ID = metadata["id"]
-            imagePath = os.path.join( imagesDir, str( int(ID) % 1000 ).zfill(4), ID + '.jpg' )
+                metadata = json.loads(line)
+                ID = metadata["id"]
+                imagePath = os.path.join( imagesDir, str( int(ID) % 1000 ).zfill(4), ID + '.jpg' )
         
-            if os.path.exists(imagePath):
-                #print("(", found + missed, "/", total, "): ",  "found [", imagePath, "]; saving matadata...")
-                found += 1
-                newMetaFile.write(line)
-            else:
-                missed += 1
-
-            #if ((found + missed) % 1000) == 0:
-                #print(found + missed, "; ",  imagePath)
-        metaFile.close()
-        newMetaFile.close()
-
-        foundTotal += found
-        found  = 0
-        missedTotal += missed
-        missed = 0
-
-    print("found ", foundTotal, " files in ", foundTotal + missedTotal, "lookups")
-
-_PROCESSED_TAGS_ = True
-if not _PROCESSED_TAGS_:
-    Lines = []
-    processedDataFiles = []
-    Tags = []
-
-
-    for filePath in os.listdir(newMetaDir):
-            processedDataFiles.append(os.path.join(newMetaDir, filePath))
-
-
-    count = 0
-    for JSON in processedDataFiles:
-        count = 0
-        File = open(JSON, 'r', encoding="utf-8")
-
-        Lines = File.readlines()
-
-        for line in Lines:
-
-            metadata = json.loads(line)
-            tags = metadata["tags"]
-
-            for tag in tags:
-                if tag['name'] not in Tags:
-                    Tags.append(tag['name'])
-                    count += 1
-                    #print("added tag ", tag['name'])
-
-        File.close()
-        print(JSON, "finished processing, added", count, "tags")
-
-    print("Found", len(Tags), "unique tags, saving to allTags.txt...")
-
-    with open(".\\allTags.txt", 'w', encoding="utf-8") as fp:
-        for item in Tags:
-            # write each item on a new line
-            fp.write("%s\n" % item)
-    print("Saved.")
-
-
-_REMOVED_DEPENDENT_ = False
-
-if not _REMOVED_DEPENDENT_:
-
-    Tags = []
-    Lines = []
-    processedDataFiles=[]
-
-    tagSave = open(".\\allTags.txt", 'r', encoding="utf-8")
-    Lines = tagSave.readlines()
-
-    for line in Lines:
-        Tags.append(line.strip())
-
-    print("Starting allocation...")
-    newTags = np.asarray(Tags)
-    df = np.empty((n, len(Tags)), dtype=np.int8)
-    print("Allocated array of shape", df.shape)
-    count = 0
-    for filePath in os.listdir(newMetaDir):
-            processedDataFiles.append(os.path.join(newMetaDir, filePath))
-
-
-    for JSON in processedDataFiles:
-        File = open(JSON, 'r', encoding="utf-8")
-
-        Lines = File.readlines()
-        lastRam = 0
-        for line in Lines:
-            count += 1
-            metadata = json.loads(line)
-            tags = metadata["tags"]
-            #row = []
-            heldTags = []
-            for hasTag in tags:
-                heldTags.append(hasTag['name'])
-            tagIndex = 0
-            for tag in Tags:
-                if tag in hasTag:
-                    #row.append(1)
-                    df[count, tagIndex] = 1
+                if os.path.exists(imagePath):
+                    #print("(", found + missed, "/", total, "): ",  "found [", imagePath, "]; saving matadata...")
+                    found += 1
+                    newMetaFile.write(line)
                 else:
-                    #row.append(0)
-                    df[count, tagIndex] = 0
-                tagIndex += 1
-            #np.vstack((df, np.asarray(row)))
+                    missed += 1
 
-            RAMUsage = psutil.virtual_memory().percent
-            if (RAMUsage*100)%5 == 0 and (RAMUsage - lastRam) >= 0.05:
-                lastRam = RAMUsage
-                print(RAMUsage, "% of RAM used for", count, "lines, current shape:", df.shape)
-                print(df[:,:])
+                #if ((found + missed) % 1000) == 0:
+                    #print(found + missed, "; ",  imagePath)
+            metaFile.close()
+            newMetaFile.close()
 
-            if RAMUsage >= 75:
-                print("Memory threshold reached, stopping dataframe generation...")
-                break
-            #TODO add row to matrix
+            foundTotal += found
+            missedTotal += missed
+            print("found", found, "images...")
+        print("found ", foundTotal, " files in ", foundTotal + missedTotal, "lookups")
+        n = foundTotal
 
-        File.close()
-        print(JSON, "finished processing, added", count, "/ 340k rows, resulting in a matrix of shape:", df.shape)
+    _GENERATE_TAGS_ = False
+    if _GENERATE_TAGS_:
+        Lines = []
+        processedDataFiles = []
+        Tags = []
+
+
+        for filePath in os.listdir(newMetaDir):
+                processedDataFiles.append(os.path.join(newMetaDir, filePath))
+
+
+        count = 0
+        for JSON in processedDataFiles:
+            count = 0
+            File = open(JSON, 'r', encoding="utf-8")
+
+            Lines = File.readlines()
+
+            for line in Lines:
+
+                metadata = json.loads(line)
+                tags = metadata["tags"]
+
+                for tag in tags:
+                    if tag['name'] not in Tags:
+                        Tags.append(tag['name'])
+                        count += 1
+                        #print("added tag ", tag['name'])
+
+            File.close()
+            print(JSON, "finished processing, added", count, "tags")
+
+        print("Found", len(Tags), "unique tags, saving to allTags.txt...")
+
+        with open(".\\allTags.txt", 'w', encoding="utf-8") as fp:
+            for item in Tags:
+                # write each item on a new line
+                fp.write("%s\n" % item)
+        print("Saved.")
+
+
+    _GENERATE_Y_ = False
+
+    if _GENERATE_Y_:
+
+        Tags = []
+        Lines = []
+        processedDataFiles=[]
+
+        tagSave = open(".\\allTags.txt", 'r', encoding="utf-8")
+        Lines = tagSave.readlines()
+        tagSave.close()
+
+        for line in Lines:
+            Tags.append(line.strip())
+
+        print("Starting allocation...")
+        newTags = np.asarray(Tags)
+        df = np.empty((n, len(Tags)), dtype=np.int8)
+        print("Allocated array of shape", df.shape)
+        count = 0
+        for filePath in os.listdir(newMetaDir):
+                processedDataFiles.append(os.path.join(newMetaDir, filePath))
+
+
+        for JSON in processedDataFiles:
+            File = open(JSON, 'r', encoding="utf-8")
+
+            Lines = File.readlines()
+            lastRam = 0
+            for line in Lines:
+                
+                metadata = json.loads(line)
+                tags = metadata["tags"]
+                #row = []
+                heldTags = []
+                for hasTag in tags:
+                    heldTags.append(hasTag['name'])
+                tagIndex = 0
+                tagCount = 0
+                for tag in Tags:
+                    if tag in heldTags:
+                        #row.append(1)
+                        df[count, tagIndex] = 1
+                    else:
+                        #row.append(0)
+                        df[count, tagIndex] = 0
+                    tagIndex += 1
+                #np.vstack((df, np.asarray(row)))
+                count += 1
+                RAMUsage = psutil.virtual_memory().percent
+                if (RAMUsage*100)%5 == 0 and (RAMUsage - lastRam) >= 0.05:
+                    lastRam = RAMUsage
+                    print(RAMUsage, "% of RAM used for", count, "lines, current shape:", df.shape)
+                    #print(df[:,:])
+
+                if RAMUsage >= 75:
+                    print("Memory threshold reached, stopping dataframe generation...")
+                    break
+                #TODO add row to matrix
+
+            File.close()
+            print(JSON, "finished processing, added", count)
         with open('Y.npy', 'wb') as f:
             np.save(f, df)
-    #print("calculating independent tags...")
-    #_, inds = sympy.Matrix(df).rref()
-    #newTags = newTags[inds]
-    #print("found", newTags.size(), "indepenent tags, saving...")
-    #goodTagSave = open(".\\independentTags.txt", 'w', encoding="utf-8")
-    #goodTagSave.writelines(Tags.tolist())
-    #goodTagSave.close()
-    print("Done.")
+
+
+        ######################## BREAKS, NOT ENOUGH RAM ########################
+        #print("calculating independent tags...")
+        #_, inds = sympy.Matrix(df).rref()
+        #newTags = newTags[inds]
+        #print("found", newTags.size(), "indepenent tags, saving...")
+        #goodTagSave = open(".\\independentTags.txt", 'w', encoding="utf-8")
+        #goodTagSave.writelines(Tags.tolist())
+        #goodTagSave.close()
+        ########################################################################
+
+        print("Done.")
+
+    _GENERATE_IMAGE_LIST_ = False
+
+    if _GENERATE_IMAGE_LIST_:
+        Images = []
+        processedDataFiles = []
+
+        for filePath in os.listdir(newMetaDir):
+                processedDataFiles.append(os.path.join(newMetaDir, filePath))
+
+        totalCount = 0
+        count = 0
+        for JSON in processedDataFiles:
+            File = open(JSON, 'r', encoding="utf-8")
+
+            totalCount += count
+            count = 0
+
+            Lines = File.readlines()
+            for line in Lines:
+
+                count += 1
+                metadata = json.loads(line)
+
+                ID = metadata["id"]
+                imagePath = os.path.join( imagesDir, str( int(ID) % 1000 ).zfill(4), ID + '.jpg' )
+                Images.append(imagePath)
+
+            File.close()
+            print(JSON, "finished processing, added", count, " images to list")
+
+        print("found", totalCount, "images, saving paths to [Images.txt]...")
+        with open(".\\Images.txt", 'w', encoding="utf-8") as fp:
+            for path in Images:
+                # write each item on a new line
+                fp.write("%s\n" % path)
+
+        with open('Images.npy', 'wb') as f:
+                np.save(f, Images)
+
+        print("Saved.")
+
+    _SPLIT_ = True
+
+    if _SPLIT_:
+        print("loading...")
+        images = np.load('Images.npy')
+        tags_hot = np.load('Y.npy')
+    
+        ################ TOO MUCH MEMORY #####################
+        #from sklearn.model_selection import train_test_split
+        #
+        #print("splitting...")
+        #X_train, X_val, y_train, y_val = train_test_split(
+        #    images, tags_hot, test_size=0.2 #, random_state=0 if need reproducable
+        #    )
+        ################################################
+
+        def shuffle(X, Y, test_proportion):
+            ratio = int(X.shape[0]/test_proportion) #should be int
+            X_train = X[ratio:]
+            X_val =  X[:ratio]
+            y_train = Y[ratio:,:]
+            y_val =  Y[:ratio,:]
+            return X_train, X_val, y_train, y_val
+
+        X_train, X_val, y_train, y_val = shuffle(images, tags_hot, 5)
+
+        print("saving...")
+        np.save('./split/X_train.npy', X_train)
+        np.save('./split/X_val.npy', X_val)
+        np.save('./split/y_train.npy', y_train)
+        np.save('./split/y_val.npy', y_val)
+        print('train/val split complete')
+
+_TRAIN_ = True
+
+if _TRAIN_:
+    
+    import keras
+    import tensorflow as tf
+    from tensorflow import keras
+    from tensorflow.keras import layers
+    from skimage.io import imread
+
+    class My_Custom_Generator(keras.utils.Sequence) :
+  
+        def __init__(self, image_filenames, labels, batch_size) :
+            self.image_filenames = image_filenames
+            self.labels = labels
+            self.batch_size = batch_size
+    
+    
+        def __len__(self) :
+            return (np.ceil(len(self.image_filenames) / float(self.batch_size))).astype(np.int)
+  
+  
+        def __getitem__(self, idx) :
+            batch_x = self.image_filenames[idx * self.batch_size : (idx+1) * self.batch_size]
+            batch_y = self.labels[idx * self.batch_size : (idx+1) * self.batch_size]
+    
+            return np.array([
+                    imread(str(file_name))
+                        for file_name in batch_x])/255.0, np.array(batch_y)
+
+    
+    def makeModel(outSize):
+
+        keras.backend.clear_session()
+
+        inputs = keras.Input(shape=(512,512,3)) #(512, 512, 3)
+
+        x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(inputs) # (510, 510, 32)
+        x = layers.MaxPooling2D(pool_size=2)(x) # (260, 260, 32)
+        x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x) # (258, 258, 64)
+        x = layers.MaxPooling2D(pool_size=2)(x) # (129, 129, 64)
+        x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x) # (127, 127, 128)
+        x = layers.MaxPooling2D(pool_size=2)(x) # (63, 63, 128)
+        x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x) # (61, 61, 128)
+        x = layers.MaxPooling2D(pool_size=2)(x) # (30, 30, 128)
+        x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x) # (28, 28, 128)
+        x = layers.MaxPooling2D(pool_size=2)(x) # (14, 14, 128)
+
+        x = layers.Flatten()(x)
+        x = layers.Dropout(0.15)(x)
+        x = layers.Dense(64, activation='relu')(x)
+        x = layers.Dense(32, activation='relu')(x)
+
+        outputs = layers.Dense(outSize, activation="softmax")(x)
+
+        return keras.Model(inputs=inputs, outputs=outputs)
+
+    print("loading data...")
+    X_train = np.load('./split/X_train.npy')
+    X_val = np.load('./split/X_val.npy')
+    y_train = np.load('./split/y_train.npy')
+    y_val = np.load('./split/y_val.npy')
+
+    #print(X_train.shape)
+    #print(X_train.shape[0])
+    #print(X_val.shape)
+    #print(X_val.shape[0])
+    #print(y_train.shape)
+    #print(y_train.shape[0])
+    #print(y_train.shape[1])
+    #print(y_val.shape)
+    #print(y_val.shape[0])
+    #print(y_val.shape[1])
+
+    batch_size = 32
+    
+    my_training_batch_generator = My_Custom_Generator(X_train, y_train, batch_size)
+    my_validation_batch_generator = My_Custom_Generator(X_val, y_val, batch_size)
+    
+    print("loading complete, making model...")
+    
+    #print(np.array(imread(str(X_train[0]))).shape)
+
+    model = makeModel(y_train.shape[1])
+    model.summary()
+    #input()
+    
+    model.compile(optimizer='rmsprop',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+    
+    callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath="callback.keras",
+        save_best_only=True,
+        monitor="val_accuracy")
+    ]
+    
+    
+    print("training start")
+    
+    model.fit_generator(generator=my_training_batch_generator,
+                   steps_per_epoch = int(X_train.shape[0] // batch_size),
+                   epochs = 50,
+                   verbose = 1,
+                   validation_data = my_validation_batch_generator,
+                   validation_steps = int(X_val.shape[0] // batch_size))
+    
+    print("saving model")
+    
+    keras.save('model.kmodel')
+    
+    print('Done.')
